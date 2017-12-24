838c40d78dfddd21d22ad16c07e4006230ec639b
DESKTOP-MS2VGA5\10603 <12589090@sina.com>
2017-12-24 21:02:37 +0800

    增加付款申请单财务人员维护界面

--- Analysis/src/main/java/com/kime/action/DictAction.java
+++ Analysis/src/main/java/com/kime/action/DictAction.java
@@ -5,6 +5,7 @@ import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.jasper.tagplugins.jstl.core.When;
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.ParentPackage;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -194,6 +195,92 @@ public class DictAction extends ActionBase{
 		return SUCCESS;
 		
 	}
+	
+	
+	
+	@Action(value="modeFinanceDict",results={@org.apache.struts2.convention.annotation.Result(type="stream",
+			params={
+					"inputName", "reslutJson"
+			})})
+	public String modeFinanceDict() throws UnsupportedEncodingException{
+		
+		List<Dict> list=new Gson().fromJson(json, new TypeToken<ArrayList<Dict>>() {}.getType());
+		Dict object=list.get(0);
+		
+		if (object.equals("Fixed Asset 固定资产")) {
+			object.setKey("1");
+		}else if (object.equals("Raw Material 原材料")) {
+			object.setKey("2");
+		}else if (object.equals("Consumable 消耗品")) {
+			object.setKey("3");
+		}else if (object.equals("Subcontractor 外包")) {
+			object.setKey("4");
+		}else if (object.equals("Service 服务")) {
+			object.setKey("5");
+		}else if (object.equals("Petty Cash备用金")) {
+			object.setKey("6");
+		}else if (object.equals("Other 其他")) {
+			object.setKey("7");
+		}
+		
+		try {
+
+			dictBIZ.update(object);
+			logUtil.logInfo("修改付款签核对应财务:"+object.getType()+" "+object.getKey());
+			
+			result.setMessage(Message.SAVE_MESSAGE_SUCCESS);
+			result.setStatusCode("200");
+		} catch (Exception e) {
+			logUtil.logInfo("修改付款签核对应财务:"+e.getMessage());
+			result.setMessage(e.getMessage());
+			result.setStatusCode("300");	
+		}
+
+		reslutJson=new ByteArrayInputStream(new Gson().toJson(result).getBytes("UTF-8"));  
+		return SUCCESS;
+		
+	}
+	
+	
+	
+	@Action(value="getFinanceDict",results={@org.apache.struts2.convention.annotation.Result(type="stream",
+			params={
+					"inputName", "reslutJson"
+			})})
+	public String getFinanceDict() throws UnsupportedEncodingException{
+		
+		List<Dict> list=dictBIZ.getDict(" WHERE TYPE='PAYMENT' ");
+		
+		for (Dict dict : list) {
+			if (dict.getKey().equals("1")) {
+				dict.setKey("Fixed Asset 固定资产");
+			}else if (dict.getKey().equals("2")) {
+				dict.setKey("Raw Material 原材料");
+			}else if (dict.getKey().equals("3")) {
+				dict.setKey("Consumable 消耗品");
+			}else if (dict.getKey().equals("4")) {
+				dict.setKey("Subcontractor 外包");
+			}else if (dict.getKey().equals("5")) {
+				dict.setKey("Service 服务");
+			}else if (dict.getKey().equals("6")) {
+				dict.setKey("Petty Cash备用金");
+			}else if (dict.getKey().equals("7")) {
+				dict.setKey("Other 其他");
+			}
+			
+		}
+		
+		reslutJson=new ByteArrayInputStream(new Gson().toJson(list).getBytes("UTF-8"));  
+		
+		logUtil.logInfo("查询付款签核对应财务:");
+		return SUCCESS;
+		
+	}
+	
+	
+	
+	
+	
 	
 	
 }
